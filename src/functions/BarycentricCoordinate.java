690cba4a102cb928d4f597b21c474fbc15f705e6
Rodrigo <ropi2203@gmail.com>
2019-04-05 16:32:53 -0300

    First part finished

--- src/functions/BarycentricCoordinate.java
+++ src/functions/BarycentricCoordinate.java
@@ -9,7 +9,14 @@ public class BarycentricCoordinate {
 		} else {
 			float[] result = new float[3];
 			
-			//??????
+			float AREAabc = Math.abs(a[0] * b[1] * 1 + a[1] * 1 * c[0] + 1 * b[0] * c[1] + (-1)*(1 * b[1] * c[0] + a[0] * 1 * c[1] + a[1] * b[0] * 1)) / 2;
+			float AREApbc = Math.abs(p[0] * b[1] * 1 + p[1] * 1 * c[0] + 1 * b[0] * c[1] + (-1)*(1 * b[1] * c[0] + p[0] * 1 * c[1] + p[1] * b[0] * 1)) / 2;
+			float AREAapc = Math.abs(a[0] * p[1] * 1 + a[1] * 1 * c[0] + 1 * p[0] * c[1] + (-1)*(1 * p[1] * c[0] + a[0] * 1 * c[1] + a[1] * p[0] * 1)) / 2;
+			float AREAabp = Math.abs(a[0] * b[1] * 1 + a[1] * 1 * p[0] + 1 * b[0] * p[1] + (-1)*(1 * b[1] * p[0] + a[0] * 1 * p[1] + a[1] * b[0] * 1)) / 2;
+			
+			result[0] = AREApbc / AREAabc;
+			result[1] = AREAapc / AREAabc;
+			result[2] = AREAabp / AREAabc;
 			
 			return result;
 		}
